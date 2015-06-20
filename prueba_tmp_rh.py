c2d64af0941c64fcf370e3f41036686c770b4e1b
jetchegoyen <juane85@gmail.com>
2015-06-20 04:15:31 +0000

    cambio en prueba temp para tomar los datos cada 5 seg

--- prueba_tmp_rh.py
+++ prueba_tmp_rh.py
@@ -1,3 +1,8 @@
+#!/usr/bin/python
 import Adafruit_DHT as dht
-h, t = dht.read_retry(dht.DHT22, 4)
-print 'Temp={0:0.1f}*C Humidity={1:0.1f}%'.format(t,h)
+import time
+
+while True:
+    h, t = dht.read_retry(dht.DHT22, 4)
+    print 'Temp={0:0.1f}*C Humidity={1:0.1f}%'.format(t,h)
+    time.sleep(5)
